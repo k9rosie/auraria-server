2302c99a491f5ee424c6f261a508ebbf62606fa1
k9rosie <k9rosie@gmail.com>
2019-03-20 04:19:50 -0500

    rthgeb

--- src/CommandHandler.js
+++ src/CommandHandler.js
@@ -13,7 +13,7 @@ export default class CommandHandler {
         this.interface.question('\n-> ', (command) => {
             let split = command.split(" ");
             if (this.commands.hasOwnProperty(split[0])) {
-                this.commands[split[0]].fn.call(this.commands[split[0]].context);
+                this.commands[split[0]].fn.call(this.commands[split[0]].context, split);
                 this.startReading();
             } else if (split[0] === "exit") {
                 console.log('Bye!\n');
